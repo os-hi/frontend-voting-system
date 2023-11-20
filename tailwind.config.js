00958b49f9df1d0b8c01255097f094f43fa51bdc
Jake <110341554+os-hi@users.noreply.github.com>
2023-11-20 21:57:40 +0800

    feat: created skeleton

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'white': '#fff',
+      'black': '#000',
+      'primary': '#7A52B3',
+      'secondary': '#00B7BF',
+      'purple': '#8461EE',
+      'accent': '#5829E5',
+      'dark': '#1F1F21',
+      'light': '#F6F8FB',
+      'gray': '#DADBDD',
+      'blue': '#4299E1',
+      'orange': '#F56565',
+      'transparent': 'transparent',
+
+    },
+  },
+  plugins: [],
+}
+
